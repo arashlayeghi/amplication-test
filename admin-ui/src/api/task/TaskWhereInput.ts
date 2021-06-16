f157d8cb7cecc8c78d9a7e534d16d86feb25dbb9
Arash Layeghi <arashlayeghi57@gmail.com>
2021-06-16 12:59:37 +0430

    amplication-build-ckpz7qx304395231bs6und7nj5n

---
+++ admin-ui/src/api/task/TaskWhereInput.ts
@@ -0,0 +1,11 @@
+import { UserWhereUniqueInput } from "../user/UserWhereUniqueInput";
+import { StringFilter } from "../../util/StringFilter";
+import { ProjectWhereUniqueInput } from "../project/ProjectWhereUniqueInput";
+import { DateTimeNullableFilter } from "../../util/DateTimeNullableFilter";
+
+export type TaskWhereInput = {
+  assignedTo?: UserWhereUniqueInput;
+  id?: StringFilter;
+  project?: ProjectWhereUniqueInput;
+  startDate?: DateTimeNullableFilter;
+};
